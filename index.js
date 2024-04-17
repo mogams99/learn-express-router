b498fc2c0a187b6873660e5f966fef5b9420faa6
Mochammad Gamal <mogams1811@gmail.com>
2024-04-17 21:09:08 +0700

    chore: :construction: adding signed cookies and sealed cookies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // * import library package
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 
 // * define port and url
 const port = 3001;
@@ -9,7 +10,17 @@ const url = `http://localhost:${port}`;
 // ? define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('secret-key'));
 
+// ! define single route
+app.get('/sign-cookie', (req, res) => {
+    res.cookie('paket', 'ransel', { signed: true });
+    res.send('signed cookie');
+});
+app.get('/verify-cookie', (req, res) => {
+    const cookies = req.signedCookies;
+    res.send(cookies);
+});
 // ? import routes
 app.use('/admin', require('./routes/admin'));
 app.use('/theater', require('./routes/theater'));
